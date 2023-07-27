dc0ed78affc2680156c881ba2a7b5190585ff682
Danielhmiller <107880080+Danielhmiller@users.noreply.github.com>
2023-07-26 20:54:19 -0500

    commits

--- frontend/src/app/components/Team_Card.js
+++ frontend/src/app/components/Team_Card.js
@@ -1,23 +1,103 @@
 import React from 'react'
 import teamStyles from '../styles/Team.module.css'
+import Image from 'next/image'
 
 export default function Team_Card() {
     return (
         <>
-            <div className={teamStyles.team_name}>Team_Card</div>
-            <div className={teamStyles.name-cards}>
-                <div class={teamStyles.name-and-rank}>
-                    <div class={Mori}>
+        <div className={teamStyles.meetus}>Meet Our Players!!!</div>
+            <div className={teamStyles.name_cards}>
+                <div className={teamStyles.name_and_rank}>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
                         <p>Mori Jin</p>
-                        <p>Bronze 1</p>
+                        <p>Radiant</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
                     </div>
-                    <div class={Maxi}>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
                         <p>Maximum Ride</p>
-                        <p>Bronze 1</p>
+                        <p>Radiant</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
                     </div>
-                    <div class={Player}>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
                         <p>PlayerUnknown</p>
-                        <p>Bronze 1</p>
+                        <p>Radiant</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
+                    </div>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
+                        <p>Railer</p>
+                        <p>Radiant</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
+                    </div>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
+                        <p>Fashion</p>
+                        <p>Radiant</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
+                    </div>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
+                        <p>Void</p>
+                        <p>Immortal III</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
+                    </div>
+                    <div className={teamStyles.member}>
+                        <div className={teamStyles.profilePic}>
+                            <Image alt="img" src="/istockphoto.jpg" fill />
+                        </div>
+                        <p>IvyBear</p>
+                        <p>Immortal III</p>
+                        <div className={teamStyles.socials}>
+                            <Image alt="img" src="/facebook.svg" width={30} height={30} />
+                            <Image alt="img" src="/instagram.svg" width={30} height={30} />
+                            <Image alt="img" src="/tiktok.svg" width={30} height={30} />
+                            <Image alt="img" src="/youtube.svg" width={30} height={30} />
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/app/team/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import teamStyles from "../styles/Team.module.css";
+import Team_Card from '../components/Team_Card';
+export default function Team() {
+  return (
+    <main>
+      <Team_Card />
+    </main>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
